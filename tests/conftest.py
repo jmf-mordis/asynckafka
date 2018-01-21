49ec3e8692f97e6db93a010140c0d3e1ea9b4b98
José Melero Fernández <jmelerofernandez@gmail.com>
2018-01-21 02:45:31 +0000

    Improved tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,5 +6,8 @@ import pytest
 
 @pytest.fixture(scope='session', autouse=True)
 def compile_project():
-    print('Compiling python project: ')
-    run('venv/bin/python setup.py build_ext --inplace', shell=True, check=True, stderr=subprocess.STDOUT)
+    print('Building asynckafka : ')
+    try:
+        run('python setup.py build_ext --inplace', shell=True, check=True, stderr=subprocess.STDOUT)
+    except Exception:
+        pytest.fail("Error building asynckafka package")
