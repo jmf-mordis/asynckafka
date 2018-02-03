2053803c68940c8a157aeea1a93b51c11520450c
José Melero Fernández <jmelerofernandez@gmail.com>
2018-02-03 16:09:11 +0000

    Added content to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 from distutils.core import setup
 from distutils.extension import Extension
+
+import os
 from Cython.Build import cythonize
 
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+    readme = f.read()
 
 setup(
+    name="asynckafka",
+    description='Fast python kafka library for asyncio.',
+    long_description=readme,
+    url='http://github.com/jmf-mordis/asynckafka',
+    license='MIT',
+    author='José Melero Fernández',
+    author_email='jmelerofernandez@gmail.com',
+    platforms=['*nix'],
+    version="0.0.1",
+    packages=['asynckafka'],
     ext_modules=cythonize([
         Extension(
             "asynckafka.producer",
@@ -15,5 +29,14 @@ setup(
             ["asynckafka/consumer.pyx"],
             libraries=["rdkafka"]
         ),
-    ])
+    ]),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Framework :: AsyncIO'
+    ],
+    test_suite='pytest'
 )
