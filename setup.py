d221ddca9f04ec660e165e451e6d94e0e8082ca4
José Melero Fernández <jmelerofernandez@gmail.com>
2018-02-25 16:49:16 +0000

    Preparing setup.py to upload the package to pypi

--- setup.py
+++ setup.py
@@ -6,23 +6,27 @@ import os
 import sys
 from Cython.Build import cythonize
 
-with open(os.path.join(os.path.dirname(__file__), '_README.rst')) as f:
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
     readme = f.read()
 
+version = '0.1.0'
+module_name = 'asynckafka'
+github_username = 'jmf-mordis'
+
 extensions = [
-    "asynckafka.settings",
-    "asynckafka.callbacks",
-    "asynckafka.utils",
-    "asynckafka.consumer.message",
-    "asynckafka.consumer.rd_kafka_consumer",
-    "asynckafka.consumer.consumer",
-    "asynckafka.producer.rd_kafka_producer",
-    "asynckafka.producer.producer",
+    'asynckafka.settings',
+    'asynckafka.callbacks',
+    'asynckafka.utils',
+    'asynckafka.consumer.message',
+    'asynckafka.consumer.rd_kafka_consumer',
+    'asynckafka.consumer.consumer',
+    'asynckafka.producer.rd_kafka_producer',
+    'asynckafka.producer.producer',
 ]
 
-if "--tests" in sys.argv:
-    extensions.append("tests.asynckafka_tests")
-    sys.argv.remove("--tests")
+if '--tests' in sys.argv:
+    extensions.append('tests.asynckafka_tests')
+    sys.argv.remove('--tests')
 
 
 module_list = [
@@ -35,22 +39,24 @@ module_list = [
 ]
 
 setup(
-    name="asynckafka",
-    packages=['asynckafka'],
+    name=module_name,
+    packages=[module_name],
     description='Fast python kafka client for asyncio.',
     long_description=readme,
-    url='http://github.com/jmf-mordis/asynckafka',
+    url=f'http://github.com/{github_username}/{module_name}',
     license='mit',
     author='José Melero Fernández',
     author_email='jmelerofernandez@gmail.com',
     platforms=['*nix'],
-    version="0.1.0",
+    version=version,
+    download_url=f'https://github.com/{github_username}/{module_name}/archive/'
+                 f'{version}.tar.gz',
     ext_modules=cythonize(
         module_list,
         compiler_directives={'embedsignature': True}
     ),
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 2 - Alpha',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: MIT License',
